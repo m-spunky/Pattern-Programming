c650b1327c4ab6da928123859e7f48883eb9ccba
m-spunky <mayurdjadhav1@gmail.com>
2023-05-05 14:39:35 +0530

    Shape Patterns

---
+++ cross.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+int i,j,k,n;
+
+n=5;
+for(i=1;i<=((2*n)-1);i++){
+
+    for(j=1;j<=((2*n)-1);j++){
+        if(i==j||j==((2*n)+1-i)){
+             printf("*");
+        }
+        else{
+        printf(" ");
+        };
+       
+    };
+
+    printf("\n");
+};
+
+return 0;
+};
+
+// *       *
+//  *     * 
+//   *   *  
+//    * *   
+//     *    
+//    * *   
+//   *   *  
+//  *     * 
+// *       *
